71cd057454760df4ce58144ca57fe7060aff19ea
macbook <elmirbekalisherov24@gmail.com>
2022-07-15 06:53:49 +0600

    Initial commit

---
+++ src/main/java/peaksoft/ProjectOnlineStoreWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package peaksoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOnlineStoreWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOnlineStoreWithSpringBootApplication.class, args);
+	}
+
+}
